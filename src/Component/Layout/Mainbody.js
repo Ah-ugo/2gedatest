da5f87444f5139866ab32130effb1899a419e430
Ah-ugo <ahuekweprinceugo@gmail.com>
2024-01-31 14:25:51 +0100

    missed

--- src/Component/Layout/Mainbody.js
+++ src/Component/Layout/Mainbody.js
@@ -4,7 +4,7 @@ export default function Mainbody({ children }) {
   return (
     <div
       style={{ overflowX: "hidden" }}
-      className="w-full h-full bg-neutral-100 mt-24">
+      className="w-full h-full bg-neutral-100 mt-24 md:mt-0">
       {children}
     </div>
   );
